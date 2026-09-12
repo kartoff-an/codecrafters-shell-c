7c5a4048a350a896784efc3cb4b8c790501e9367
Kartoffan <reanglenn.roquero@gmail.com>
2026-09-12 21:56:54 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,15 +6,17 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
+  while (1) {
+    printf("$ ");
 
-  // reading the user's command
-  char input[100];
-  fgets(input, sizeof(input), stdin);
+    // reading the user's command
+    char input[100];
+    fgets(input, sizeof(input), stdin);
 
-  // printing the input not found message
-  input[strcspn(input, "\n")] = '\0';
-  printf("%s: command not found\n", input);
+    // printing the input not found message
+    input[strcspn(input, "\n")] = '\0';
+    printf("%s: command not found\n", input);
+  }
 
   return 0;
 }
