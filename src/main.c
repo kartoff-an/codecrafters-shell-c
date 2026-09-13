3580ff1c4eb9a8eb9b5aa40b700ee0208b3ebc12
Kartoffan <reanglenn.roquero@gmail.com>
2026-09-13 17:08:13 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,28 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_COMMAND_LENGTH 100
+
+int find_type(char* type, const char* types[], int n_types) {
+  for (int i = 0; i < n_types; i++) {
+    if (strcmp(types[i], type) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  while (1) {
+  char command[MAX_COMMAND_LENGTH];
+  const char* types[] = {"echo", "exit", "type"};
+  int types_size = sizeof(types) / sizeof(types[0]);
+
+  int should_exit = 0;
+  while (!should_exit) {
     printf("$ ");
 
-    // reading the user's command
-    char input[100];
-    fgets(input, sizeof(input), stdin);
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0';
+    char *builtin = strtok(command, " ");
+    char *arg = strtok(NULL, "");
 
-    // printing the input not found message
-    input[strcspn(input, "\n")] = '\0';
-
-    if (strcmp(input, "exit") == 0) {
-      break;
+    if (builtin == NULL) {
+      continue;
     }
 
-    if (strncmp(input, "echo ", 5) == 0) {
-      printf("%s\n", input + 5);
-    } else {
-      printf("%s: command not found\n", input);
+    if (strcmp(builtin, "exit") == 0) {
+      break;
+    }
+    else if (strcmp(builtin, "echo") == 0) {
+      printf("%s\n", arg);
+    }
+    else if (strcmp(builtin, "type") == 0) {
+      if (find_type(builtin, types, types_size) >= 0) {
+        printf("%s is a shell builtin\n", arg);
+      }
+      else {
+        printf("%s: not found\n", arg);
+      }
+    }
+    else {
+      printf("%s: command not found\n", builtin);
     }
   }
 
